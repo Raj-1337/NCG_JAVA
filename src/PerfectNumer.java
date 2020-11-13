0bbb9436abd152d4d3628319e616b887ffb289ad
Raj-1337 <srk1998007@gmail.com>
2020-11-14 01:21:08 +0530

    aded CE 22, PerfectNumer

---
+++ src/PerfectNumer.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PerfectNumer {
+
+    public static boolean isPerfectNumber(int num) {
+        if (num < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int  i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(isPerfectNumber(scanner.nextInt()));
+    }
+
+}
